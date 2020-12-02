874cf69a6a037ac64d001d6b206a6e1d1e2af86b
Stephen Synchronicity <sholsen@alumni.emory.edu>
2020-12-02 15:35:13 -0500

    Clock: remove v, use get_url

--- R/Clock.R
+++ R/Clock.R
@@ -6,7 +6,6 @@
 #' 
 #' @description The clock API serves the current market timestamp, whether or not the market is currently open, as well as the times of the next market open and close. See [Clock](https://alpaca.markets/docs/api-documentation/api-v2/clock/) Endpoint for details. *Additionally, if your current timezone is different than that of the market* or if a `tz` is specified, a `local` POSIXct will be appended with corresponding local time (or `tz` time) and it's `offset` from the market for each of the following outputs: `timestamp`, `next_open`, and `next_close`.
 #' @param tz `(character)` A timezone (See \code{\link[base]{timezones}} or use `OlsonNames()`)  to determine how the market hours compare to the `tz` hours. If no `tz` argument is provided, and the local timezone differs from "America/New_York", `clock` will automatically provide the local conversion and offset. 
-#' @inheritParams account
 #' @return Clock \code{(list)} A [Clock](https://alpaca.markets/docs/api-documentation/api-v2/clock/#clock-entity) Object:
 #' \itemize{
 #'  \item{\code{timestamp}}{ \code{(POSIXct)} Current timestamp.}
@@ -20,14 +19,14 @@
 #' @importFrom lubridate as_datetime with_tz is.POSIXct force_tz `.__T__-:base`
 #' @importFrom purrr map_if
 #' @export
-clock <- function(tz = NULL, v = 2){
+clock <- function(tz = NULL){
   .tz <- ifelse(is.null(tz), Sys.timezone(), tz)
   #Set URL & Headers
-  .url = get_url()
+  
   headers = get_headers()
   `-` <- lubridate::`.__T__-:base`
   #Send Request
-  cl = httr::GET(url = paste0(.url,"/",paste0("v",v),"/clock"), headers)
+  cl = httr::GET(url = get_url("clock"), headers)
   cl = response_text_clean(cl)
   suppressMessages({
   cl <- purrr::map_if(cl, is.character, lubridate::as_datetime, tz = "America/New_York")
@@ -40,12 +39,5 @@ clock <- function(tz = NULL, v = 2){
   })
   return(cl)
 }
-#----------------------------------------------------------------------------------------------
-#' @family Clock
-#' @rdname clock
-#' @title get_clock (Deprecated)
-#' @description `get_clock` is deprecated, use \code{\link[AlpacaforR]{clock}} instead.
-#' @examples get_clock()
-#' @export
-get_clock <- clock
+
 
