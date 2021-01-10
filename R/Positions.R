b03d6148c44c5a7d575d18f445a25ba130413928
Stephen Synchronicity <sholsen@alumni.emory.edu>
2021-01-10 17:58:21 -0500

    Simplify pos_transform, add query comparable to orders/market_data
    - `ticker` to `symbol`
    - instead of stripping the first two elements when `action = "close_all"` they are now combined into the outgoing `tibble`.
     - vectorized `message` and `warning` not dependent on `messages` in other functions now
     - more robust detection of endpoint
     - implement the new `query = cancel_orders` flag to cancel all pending orders as well (code that did that manually now removed)
    - fewer conditionals (yay!)

--- R/Positions.R
+++ R/Positions.R
@@ -4,15 +4,15 @@
 #' @title Get Positions
 #'
 #' @description The positions API provides information about an account's current open positions. The response will include information such as cost basis, shares traded, and market value, which will be updated live as price information is updated. Character values are returned as a string while integer values are returned as numeric. See [Positions](https://alpaca.markets/docs/api-documentation/api-v2/positions/) for details.
-#' @param tickers `(character)` Symbol(s) for which Position information will be retrieved. 
+#' @param symbols `(character)` Symbol(s) for which Position information will be retrieved. 
 #' @param action `(character)` 
 #' \itemize{
-#'   \item{\code{"get"/"g"}}{ \code{GET} all positions or those specified by `tickers`}
-#'   \item{\code{"close","c"}}{ Close positions specified by `tickers`}
+#'   \item{\code{"get"/"g"}}{ \code{GET} all positions or those specified by `symbols`}
+#'   \item{\code{"close","c"}}{ Close positions specified by `symbols`}
 #'   \item{\code{"close_all"}}{ Close all positions}
 #' }
 #' @inheritParams account
-#' @details `position` is vectorized and thus multiple arguments may be provided to `tickers` and the function will perform the `action` specified for each. `action` is not vectorized, and only one action may be performed for a set of tickers at a given time.
+#' @details `position` is vectorized and thus multiple arguments may be provided to `symbols` and the function will perform the `action` specified for each. `action` is not vectorized, and only one action may be performed for a set of symbols at a given time.
 #' @return Position `(tibble)` [Position object](https://alpaca.markets/docs/api-documentation/api-v2/positions/#position-entity) or array of Position objects of length 0 if no positions, otherwise with length of the number of positions, each with the following attributes: 
 #'\itemize{
 #' \item{`asset_id`}{`(character)` Asset ID.}
@@ -33,7 +33,7 @@
 #' \item{`change_today`}{`(numeric)` Percent change from last day price (by a factor of 1).}
 #' }
 #' @examples 
-#' positions(ticker = "AAPL", live = FALSE)
+#' positions("AAPL", live = FALSE)
 #' positions("AAPL", live = TRUE)
 #' positions() # all paper positions
 #' positions(live = TRUE) # all live positions
@@ -43,11 +43,11 @@
 #' positions(a = "close_all")
 #' @export
 positions <-
-  function(tickers = NULL,
+  function(symbols = NULL,
            action = "get",
            live = as.logical(Sys.getenv("APCA-LIVE", FALSE))) {
     
-  if (is.character(tickers)) tickers <- toupper(tickers)
+  if (is.character(symbols)) symbols <- toupper(symbols)
   
   .all <- grepl("close_all", action, ignore.case = TRUE)
   if (!.all) {
@@ -57,17 +57,18 @@ positions <-
   .url <- get_url("positions", live = live)
   headers = get_headers(live)
   if (.all) {
+    .url <- get_url("positions", query = list(cancel_orders = TRUE), live = live)
     #Send Request
     pos = httr::DELETE(url = .url, headers)
     out <- pos_transform(pos)
-  } else if (length(tickers) > 0 && action %in% c("close", "get")) {
+  } else if (length(symbols) > 0 && action %in% c("close", "get")) {
     if (action == "get") {
       .expr <- rlang::expr({pos = httr::GET(url = .url, headers)})
     } else if (action == "close") {
       .expr <- rlang::expr({pos = httr::DELETE(url = .url, headers)})
     }
-    # if a list of tickers is specified, close or fetch each.
-    out <- purrr::map_dfr(tickers, ~{
+    # if a list of symbols is specified, close or fetch each.
+    out <- purrr::map_dfr(symbols, ~{
       .url <- get_url(c("positions", .x), live = live)
       #Send Request
       eval(.expr)
@@ -88,68 +89,64 @@ positions <-
 #' @keywords internal
 pos_transform <- function(pos) {
   
-  if (class(pos) != "response") {
-    .code <- pos$code
-    .message <- pos$message
-  } else if (class(pos) == "response") {
+  if (class(pos) == "response") {
     .method <- pos$request$method
-    .sym <- stringr::str_extract(pos$request$url, "\\w+$")
-    .code <- pos$status_code
-    #browser()
+    .url <- httr::parse_url(pos$url)
+    .sym <- stringr::str_extract(.url$path, "\\w+$")
+    .q <- list(ts = lubridate::with_tz(pos$date, Sys.timezone()),
+         status_code = pos$status_code,
+         url = pos$url)
     .pos <- response_text_clean(pos)
     .message <- .pos$message
+  } else {
+    stop("`*_transform` requires a response object.")
   }
   
-  # if related orders
-  .held <- !is.null(.pos$body$held_for_orders)
-  if (.held) {
-    # close all orders. As of 2020-05-15, all open related orders must be canceled to close positions
-    # This should properly return orders
-    message("Related orders prevent positions from being closed. Canceling related orders...")
-    .pos <- purrr::pmap(.pos$body, ~{
-      .vars <- list(...)
-      if (is.null(.vars$related_orders)) return(NULL)
-      .e <- new.env()
-      withCallingHandlers(message = function(e) {
-        if (e$message == "Order canceled successfully\n") 
-          rlang::warn(message = paste0("Canceled order ", .vars$related_orders," for ", .vars$symbol)) 
-        else
-          rlang::warn(message = paste0("Could not cancel order ", .vars$related_orders," for ", .vars$symbol, ". Position for ", .vars$symbol, " remains open."))
-      }, {
-        .out <- order_submit(.vars$related_orders, action = "cancel")
-        .out <- positions(.vars$symbol, a = "c")
-      })
-      return(.out)
-    })
-    # order_transform should properly transform orders
-    
-  }
-  
-  if(any(grepl(pattern = "^4", x = .code))) {
+  # check for error
+  if(any(grepl(pattern = "^4", x = .q$status_code))) {
     rlang::warn(paste("Position was not",ifelse(grepl("GET", .method, ignore.case = TRUE), "found.", "modified."),"\n Message:", .message))
     return(.pos)
-  } else if (.sym != "positions" && .sym %in% .pos$symbol && grepl("DELETE", .method, ignore.case = TRUE)) {
-    message(paste0(.sym, " closed successfully."))
-  } else if (grepl("DELETE", .method, ignore.case = TRUE) && any(grepl("^2", .pos$body$code %||% .pos$status))) {
-    message(paste0("All positions closed successfully.\nClosed Position(s): ", paste0(.pos$body$sym[grepl("^2", .pos$body$code %||% .pos$status)], collapse = ", ")))
   }
   
-  #Check if any pos exist before attempting to return
-  if(length(.pos) == 0) {
+  # if close_all
+  .all <- isTRUE(as.logical(.url$query$cancel_orders))
+  if (.all) {
+    out <- suppressMessages(dplyr::bind_cols(.pos[!names(.pos) %in% "body"], dplyr::rename(.pos$body, order_status = "status", order_symbol = "symbol")) %>%
+      dplyr::mutate(dplyr::across(tidyselect::ends_with("at"), lubridate::as_datetime, tz = Sys.timezone())))
+    purrr::pwalk(out, ~{
+      .vars <- list(...)
+      if (.vars$status == 200) 
+        message(paste0(
+          .vars$updated_at,
+          ": ",
+          .vars$symbol,
+          " is ",
+          .vars$order_status,
+          if (!is.null(.vars$message)) paste0("\nmessage:", .vars$message)
+              else
+                NULL
+        ))
+      else
+        rlang::warn(paste0(
+          .vars$updated_at,
+          ": ",
+          .vars$symbol,
+          " is ",
+          .vars$order_status,
+          if (!is.null(.vars$message)) paste0("\nmessage:", .vars$message)
+              else
+                NULL
+        ))
+    })
+    
+  } else if(length(.pos) == 0) {
     message("No positions are open at this time.")
     out <- .pos
-  } else if(length(.pos) > 1 && !(grepl("DELETE", .method, ignore.case = TRUE) && .sym == "positions")) {
+  } else if (length(.pos) > 1 && !(grepl("DELETE", .method, ignore.case = TRUE) && .sym == "positions")) {
     out <- order_transform(pos)
-  } else if (.sym == "positions") {
-    # if close_all
-    if (.held) {
-      out <- bind_rows(.pos)
-    } else {
-      out <- order_transform(.pos$body)
-      attr(out, "info") <- .pos[1:2]
-    }
-    
-  }
+  } 
+  
+  attr(out, "query") <- .q
   return(out)
 }
 
