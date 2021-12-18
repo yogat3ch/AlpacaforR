2e263976e047fff2646b23a1c4181caf11a20f84
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-17 20:35:53 -0500

    typo

--- R/Market_Data.R
+++ R/Market_Data.R
@@ -111,7 +111,7 @@ market_data <- function(symbol, v = 2, timeframe = "day", multiplier = 1, from =
   
   evar$tqs <- c("lt", "tr", "lq", "qu", "ss")
   evar$is_tqs <- FALSE
-  adjustment <- match_letter(adjustment, c('raw', "split", "dividend", "all"))
+  adjustment <- match_letters(adjustment, c('raw', "split", "dividend", "all"))
   
   # Process & Bind important variables:  Thu Mar 26 08:40:24 2020 ----
   evar_bind() 
