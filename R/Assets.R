2581b711902145e52739fbf050b9b0bb8473b9ef
Stephen Holsenbeck <stephen@virgalabs.io>
2023-06-03 21:27:50 -0400

    Update `assets` to accommodate new `attributes` field

--- R/Assets.R
+++ R/Assets.R
@@ -2,10 +2,10 @@
 # assets ----
 # Wed Apr 22 20:32:51 2020
 #' @family Assets
-#' @title Get Assets 
-#' 
+#' @title Get Assets
+#'
 #' @description The assets API serves as the master list of assets available for trade and data consumption from Alpaca. Assets are sorted by asset class, exchange and symbol. Some assets are only available for data consumption via Polygon, and are not tradable with Alpaca. These assets will be marked with the flag `tradable = FALSE`. See [Assets: GET](https://alpaca.markets/docs/api-documentation/api-v2/assets#get-an-asset) for details.
-#' @param ... `(character)` asset symbols or IDs. If NULL (the default), a tibble of all assets is returned. 
+#' @param ... `(character)` asset symbols or IDs. If NULL (the default), a tibble of all assets is returned.
 #' @details This function is vectorized over all arguments.
 #' @return Asset `(tibble)` [Asset](https://alpaca.markets/docs/api-documentation/api-v2/assets/#asset-entity) Object or array of Asset objects with the following parameters:
 #' \itemize{
@@ -23,7 +23,7 @@
 #'  @details Errors will be caught and surfaced but the function will always return a \code{(tibble)} to prevent error breaks when retrieving a large vector of assets.
 #' @examples
 #' \dontrun{
-#' # Get a tibble of all active assets: 
+#' # Get a tibble of all active assets:
 #' assets()
 #' # Get a specific asset by symbol:
 #' (AAPL <- assets("AAPL"))
@@ -40,24 +40,25 @@ assets <- function(...){
   } else {
     .is_id <- is_id(symbol_id)
     if (!.is_id) symbol_id <- toupper(symbol_id) # caps if ticker
-    out <- purrr::map_dfr(symbol_id, ~{
+    out <- purrr::map_dfr(symbol_id, \(.x) {
+      resp <- httr::GET(url = get_url(c("assets", .x)), headers)
       # get response
       asts = try(asset_transform(httr::GET(url = get_url(c("assets", .x)), headers)), silent = TRUE)
       if (is_error(asts)) {
         rlang::warn(attr(asts,"condition")$message)
         asts <- data.frame(symbol = .x, status = "Not Found")
-      } 
+      }
       asts
     })
   }
-  
+
   return(out)
 }
 
 
 #' @title asset_transform
 #' @description Transform API response to asset tibble
-#' @param asts \code{(response)} 
+#' @param asts \code{(response)}
 #' @return \code{(tibble)}
 #' @keywords Internal
 #' @importFrom tibble as_tibble
@@ -67,17 +68,33 @@ asset_transform <- function(asts) {
   if (!is.null(out$code)) {
     rlang::warn(out$message)
     out <- tibble::tibble(
-      id = NA,
-      class = NA,
-      exchange = NA,
+      id = NA_character_,
+      class = NA_character_,
+      exchange = NA_character_,
       symbol = basename(asts$url),
-      name = "",
+      name = NA_character_,
       status = out$message,
       tradeable = FALSE,
-      marginable = FALSE
+      marginable = FALSE,
+      maintenance_margin_requirement = NA_integer_,
+      shortable = FALSE,
+      easy_to_borrow = FALSE,
+      fractionable = FALSE,
+      attributes = list(),
+      min_order_size = NA_real_,
+      min_trade_increment = NA_real_,
+      price_increment = NA_real_
     )
   } else {
-    out <- tibble::as_tibble(out)
+    q <- attr(out, "query")
+    # Some items are 0 length lists. These must be wrapped in list to fit into a single row tibble
+    i_list <- purrr::map_lgl(out, rlang::is_list)
+    li <- out[i_list]
+    out <- tibble::tibble_row(!!!out[!i_list])
+    for (nm in names(li)) {
+      out[[nm]] <- list(li[[nm]])
+    }
+    attr(out, "query") <- q
   }
   out
 }
